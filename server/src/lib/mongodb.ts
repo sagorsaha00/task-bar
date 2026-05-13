5e2fdd8a6b21879b91395b7589c9bd1ed478ffa0
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-05-13 21:18:35 +0600

    dns

--- server/src/lib/mongodb.ts
+++ server/src/lib/mongodb.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dns from "node:dns"
+dns.setServers(["8.8.8.8", "8.8.4.4"])
 
 export const initDb = async () => {
   try {
@@ -8,7 +10,7 @@ export const initDb = async () => {
     await mongoose.connect(
       "mongodb+srv://mrartimas24:task_manager21@cluster0.djqt5qb.mongodb.net/"
     );
-   
+
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
